bc099866e23c42badc5252dbe0ee8455a447ff3d
SUN-AAA <sunna0626@gmail.com>
2025-07-14 16:28:35 +0900

    fix: profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,13 @@
 function profile() {
     return(
-        <h3>안녕하세요 저는 정선아 입니다.</h3>
+        <div>
+            <h2>안녕하세요 저는 정선아 입니다!</h2>
+            <p><b>학력 : 조선대학교 컴퓨터공학과</b></p>
+            <p><b>사용 언어 : C, javaScript, Python</b></p>
+            <p><b>사용 툴 : Notion, Discord, Github 등</b></p>
+            <br></br>
+            <br></br>
+        </div>
     )
 }
 
