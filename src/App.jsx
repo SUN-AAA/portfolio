82fd870918d8009f1904d9306604382006481f64
SUN-AAA <sunna0626@gmail.com>
2025-07-14 16:08:42 +0900

    header 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Background from './components/background';
 import Profile from './components/profile';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Background/>
       <Profile/>
     </div>
---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+function header() {
+    return(
+        <h1>정선아 PORTFOLIO</h1>
+    )
+}
+
+export default header;
