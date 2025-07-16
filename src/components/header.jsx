b7f60726564ac84aaf084a77d449fff3ddd55f8c
SUN-AAA <sunna0626@gmail.com>
2025-07-16 16:03:11 +0900

    fix: topLeftButton to topRightButton

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,11 +3,11 @@ import "../styles/header.css";
 function header() {
     return(
         <div>
-            <div class="topLeftBox">
-                <p><a href="https://github.com/SUN-AAA" class="topLeftBoxItem"><b>Github</b></a></p>
+            <div class="topRightBox">
+                <p><a href="https://github.com/SUN-AAA" class="topRightBoxItem"><b>Github</b></a></p>
             </div>
             <div class="homebutton">
-                <p><a href="#home" class="topLeftBoxItem"><b>HOME</b></a></p>
+                <p><a href="#home" class="topRightBoxItem"><b>HOME</b></a></p>
             </div>
             <div>
                 <h1>PORTFOLIO</h1>
