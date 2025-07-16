8a4838eef5c4f5bb5323e5e63fce295b55e592e8
SUN-AAA <sunna0626@gmail.com>
2025-07-16 16:19:43 +0900

    fix: aboutme

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -10,17 +10,17 @@ function aboutme(){
             </a>
             <p><b>Mail : sun20060626@gmail.com</b></p>
             <p><b>Instagram : sunor06</b></p>
-            <p><b>Language : C, javaScript, Python, HTML, CSS</b></p>
+            <h3><b>Language</b></h3>
             <img src="https://img.shields.io/badge/c-A8B9CC?style=for-the-badge&logo=c&logoColor=white"></img>
             <img src="https://img.shields.io/badge/javascript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white"></img>
             <img src="https://img.shields.io/badge/python-3776AB?style=for-the-badge&logo=python&logoColor=white"></img>
             <img src="https://img.shields.io/badge/html5-E34F26?style=for-the-badge&logo=html5&logoColor=white"></img>
             <img src="https://img.shields.io/badge/css-663399?style=for-the-badge&logo=css&logoColor=white"></img>
-            <p><b>Tools : Notion, Discord, Github</b></p>
+            <h3><b>Tools</b></h3>
             <img src="https://img.shields.io/badge/notion-000000?style=for-the-badge&logo=notion&logoColor=white"></img>
             <img src="https://img.shields.io/badge/discord-5865F2?style=for-the-badge&logo=discord&logoColor=white"></img>
             <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"></img>
-            <p><b>Learning : React, Node.js</b></p>
+            <h3><b>Learning</b></h3>
             <img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=white"></img>
             <img src="https://img.shields.io/badge/node.js-5FA04E?style=for-the-badge&logo=node.js&logoColor=white"></img>
             <br></br>
