b7f1c618069d626c372115d2c60ebbe2e043ed40
SUN-AAA <sunna0626@gmail.com>
2025-07-16 13:56:31 +0900

    fix: components-background&profile to profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,6 +1,12 @@
+import img from '../assets/background.jpg'
+import "../styles/background.css" 
+
 function profile() {
     return(
-        <div>
+        <div id="profile">
+            <hr></hr>
+            <h2>Profile</h2>
+            <img src={img} alt = "background" className="background"></img>
             <h2>안녕하세요 저는 정선아 입니다!</h2>
         </div>
     )
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,10 +1,8 @@
-import Profile from "../components/profile";
-import Background from"../components/background";
+import Profile from"../components/profile";
 
 function profile(){
     return(
         <div id="profile">
-            <Background/>
             <Profile/>
         </div>
     )
