04d49063aa0c26ec9918d667e65afceb051306dd
SUN-AAA <sunna0626@gmail.com>
2025-07-16 13:21:26 +0900

    add: aboutme learning icon

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -3,7 +3,7 @@ function aboutme(){
         <div>
             <p><b>학력 : 조선대학교 컴퓨터공학과 1학년</b></p>
             <p>고등학생때까지 취미로 조금씩 코딩을 하다가 전문적으로 배워보고싶어서 진학하게 되었습니다.</p>
-            <p><b>Github : <a href="https://github.com/SUN-AAA">Visit</a></b></p>
+            <p><b>Github : <button><a href="https://github.com/SUN-AAA">Visit</a></button></b></p>
             <p><b>Baekjoon : Silver 5</b></p>
             <a href="https://solved.ac/profile/sunna0626">
             <img src="https://mazassumnida.wtf/api/generate_badge?boj=sunna0626" alt="Solved.ac Profile" />
@@ -19,6 +19,8 @@ function aboutme(){
             <img src="https://img.shields.io/badge/discord-5865F2?style=for-the-badge&logo=discord&logoColor=white"></img>
             <img src="https://img.shields.io/badge/github-181717?style=for-the-badge&logo=github&logoColor=white"></img>
             <p><b>Learning : React, Node.js</b></p>
+            <img src="https://img.shields.io/badge/react-61DAFB?style=for-the-badge&logo=react&logoColor=white"></img>
+            <img src="https://img.shields.io/badge/node.js-5FA04E?style=for-the-badge&logo=node.js&logoColor=white"></img>
             <br></br>
             <br></br> 
         </div>
