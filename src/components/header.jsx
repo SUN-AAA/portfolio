bd88ad877e62b30645d656b79273f43f99c66566
SUN-AAA <sunna0626@gmail.com>
2025-07-16 15:54:45 +0900

    add: home,github button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,11 @@ import "../styles/header.css";
 function header() {
     return(
         <div>
-            <div class="fixednav">
-                <p><a href="#home" class="font"><b>HOME</b></a></p>
+            <div class="topLeftBox">
+                <p><a href="https://github.com/SUN-AAA" class="topLeftBoxItem"><b>Github</b></a></p>
+            </div>
+            <div class="homebutton">
+                <p><a href="#home" class="topLeftBoxItem"><b>HOME</b></a></p>
             </div>
             <div>
                 <h1>PORTFOLIO</h1>
